458b82038cbe7d77381a2776f6cbd7bc5acbdd56
Mihai Iorga <mihai@iorga.eu>
2019-12-18 14:47:12 +0200

    Create 02_02.js

---
+++ 2019/02_02.js
@@ -0,0 +1,37 @@
+let finished = false;
+for (let x = 0; x <= 99; x++){
+    for (let y = 0; y <= 99; y++) {
+        split = document.documentElement.innerText.split(',').map(el => parseInt(el));
+        split[1] = x;
+        split[2] = y;
+
+        for(let i = 0; i<= split.length; i++) {
+            if(i % 4 === 0) {
+                if (split[i] === 99) {
+                    break;
+                }
+
+                if(split[i] === 1) {
+                    split[split[i+3]] = split[split[i+1]] + split[split[i+2]];
+                    continue;
+                }
+
+                if(split[i] === 2) {
+                    split[split[i+3]] = split[split[i+1]] * split[split[i+2]];
+                }
+            }
+        }
+
+        if (split[0] === 19690720) {
+            console.log('Result: ' + (100 * x + y));
+            finished = true;
+            break;
+        }
+    }
+    if (finished) {
+        break;
+    }
+}
+
+
+console.log(split);

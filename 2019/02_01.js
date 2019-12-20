d5d8f4ceff4b3edb61cc19e4409784bf773f9bba
Mihai Iorga <mihai@iorga.eu>
2019-12-20 10:40:27 +0200

    Update 02_01.js

--- 2019/02_01.js
+++ 2019/02_01.js
@@ -2,20 +2,20 @@ let split = document.documentElement.innerText.split(',').map(el => parseInt(el)
 split[1] = 12;
 split[2] = 2;
 
-for(let i = 0; i<= split.length; i++) {
-    if(i % 4 === 0) {
+for (let i = 0; i <= split.length; i++) {
+    if (i % 4 === 0) {
         if (split[i] === 99) {
             console.log(split);
             break;
         }
 
-        if(split[i] === 1) {
-            split[split[i+3]] = split[split[i+1]] + split[split[i+2]];
+        if (split[i] === 1) {
+            split[split[i + 3]] = split[split[i + 1]] + split[split[i + 2]];
             continue;
         }
 
-        if(split[i] === 2) {
-            split[split[i+3]] = split[split[i+1]] * split[split[i+2]];
+        if (split[i] === 2) {
+            split[split[i + 3]] = split[split[i + 1]] * split[split[i + 2]];
         }
     }
 }

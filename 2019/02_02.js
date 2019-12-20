277d1a1e830ecc2c41beb744aa833c08a02f300c
Mihai Iorga <mihai@iorga.eu>
2019-12-20 10:41:30 +0200

    Update 02_02.js

--- 2019/02_02.js
+++ 2019/02_02.js
@@ -1,24 +1,24 @@
 let finished = false;
 let check = 19690720;
-for (let x = 0; x <= 99; x++){
+for (let x = 0; x <= 99; x++) {
     for (let y = 0; y <= 99; y++) {
-        split = document.documentElement.innerText.split(',').map(el => parseInt(el));
+        let split = document.documentElement.innerText.split(',').map(el => parseInt(el));
         split[1] = x;
         split[2] = y;
 
-        for(let i = 0; i<= split.length; i++) {
-            if(i % 4 === 0) {
+        for (let i = 0; i <= split.length; i++) {
+            if (i % 4 === 0) {
                 if (split[i] === 99) {
                     break;
                 }
 
-                if(split[i] === 1) {
-                    split[split[i+3]] = split[split[i+1]] + split[split[i+2]];
+                if (split[i] === 1) {
+                    split[split[i + 3]] = split[split[i + 1]] + split[split[i + 2]];
                     continue;
                 }
 
-                if(split[i] === 2) {
-                    split[split[i+3]] = split[split[i+1]] * split[split[i+2]];
+                if (split[i] === 2) {
+                    split[split[i + 3]] = split[split[i + 1]] * split[split[i + 2]];
                 }
             }
         }
@@ -33,6 +33,3 @@ for (let x = 0; x <= 99; x++){
         break;
     }
 }
-
-
-console.log(split);
